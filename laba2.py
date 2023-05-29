dbd442f255d2b7cd675dfb6b31f7e2ab404cc1b1
ArtemKhairov <artemxairoff@icloud.com>
2023-05-29 21:49:28 +0700

    add small steps

--- laba2.py
+++ laba2.py
@@ -17,47 +17,50 @@ MAX_NUMBER_OF_CLASTERS = int(input())
 
 davies_bouldin_score_data = [[], []]
 
-for i in range(2, MAX_NUMBER_OF_CLASTERS + 1):
-    kmeans = KMeans(n_clusters=i)
+for i in range(2, MAX_NUMBER_OF_CLASTERS+1):
+    kmeans = KMeans(n_clusters=i, n_init=MAX_NUMBER_OF_CLASTERS+1)
     model = kmeans.fit_predict(df[[X, Y]])
     davies_bouldin_score_data[0].append(i)
     davies_bouldin_score_data[1].append(davies_bouldin_score(df[[X, Y]], model))
 
 davies_bouldin_figure = plt.plot(davies_bouldin_score_data[0], davies_bouldin_score_data[1])
-davies_bouldin_figure
+plt.xlabel('Количество класетров')
+plt.ylabel('Davies-Bouldin Score')
+plt.title('Davies-Bouldin Score for Different Number of Clusters')
+plt.show()
 
-USER_NUMBER_OF_CLASTERS_CHOISE = int(input())
-
-kmeans = KMeans(n_clusters=USER_NUMBER_OF_CLASTERS_CHOISE)
-kmeans.fit_predict(df[[X, Y]])
-figure = plt.scatter(df[X], df[Y], c=kmeans.labels_, cmap='rainbow')
-figure
-
-davies_bouldin_score_data = [[], []]
-
-for i in range(2, MAX_NUMBER_OF_CLASTERS + 1):
-    agglomerative = AgglomerativeClustering(n_clusters=i)
-    model = agglomerative.fit_predict(df[[X, Y]])
-    davies_bouldin_score_data[0].append(i)
-    davies_bouldin_score_data[1].append(davies_bouldin_score(df[[X, Y]], model))
-
-davies_bouldin_figure = plt.plot(davies_bouldin_score_data[0], davies_bouldin_score_data[1])
-davies_bouldin_figure
-
-
-USER_NUMBER_OF_CLASTERS_CHOISE = int(input())
-
-agglomerative = AgglomerativeClustering(n_clusters=USER_NUMBER_OF_CLASTERS_CHOISE)
-agglomerative.fit_predict(df[[X, Y]])
-figure = plt.scatter(df[X], df[Y], c=agglomerative.labels_, cmap='rainbow')
-figure
-
-
-USER_NUMBER_OF_EPS_CHOISE = int(input())
-
-dbscan = DBSCAN(eps=USER_NUMBER_OF_EPS_CHOISE)
-dbscan.fit_predict(df[[X, Y]])
-figure = plt.scatter(df[X], df[Y], c=dbscan.labels_, cmap='rainbow')
-figure
+# USER_NUMBER_OF_CLASTERS_CHOISE = int(input())
+#
+# kmeans = KMeans(n_clusters=USER_NUMBER_OF_CLASTERS_CHOISE)
+# kmeans.fit_predict(df[[X, Y]])
+# figure = plt.scatter(df[X], df[Y], c=kmeans.labels_, cmap='rainbow')
+# figure
+#
+# davies_bouldin_score_data = [[], []]
+#
+# for i in range(2, 9 + 1):
+#     agglomerative = AgglomerativeClustering(n_clusters=i)
+#     model = agglomerative.fit_predict(df[[X, Y]])
+#     davies_bouldin_score_data[0].append(i)
+#     davies_bouldin_score_data[1].append(davies_bouldin_score(df[[X, Y]], model))
+#
+# davies_bouldin_figure = plt.plot(davies_bouldin_score_data[0], davies_bouldin_score_data[1])
+# davies_bouldin_figure
+#
+#
+# USER_NUMBER_OF_CLASTERS_CHOISE = int(input())
+#
+# agglomerative = AgglomerativeClustering(n_clusters=USER_NUMBER_OF_CLASTERS_CHOISE)
+# agglomerative.fit_predict(df[[X, Y]])
+# figure = plt.scatter(df[X], df[Y], c=agglomerative.labels_, cmap='rainbow')
+# figure
+#
+#
+# USER_NUMBER_OF_EPS_CHOISE = int(input())
+#
+# dbscan = DBSCAN(eps=USER_NUMBER_OF_EPS_CHOISE)
+# dbscan.fit_predict(df[[X, Y]])
+# figure = plt.scatter(df[X], df[Y], c=dbscan.labels_, cmap='rainbow')
+# figure
 
 
---
+++ task2.py
@@ -0,0 +1,38 @@
+import numpy as np
+import plotly.express as px
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN
+from sklearn.datasets import make_blobs
+
+# Создание случайных данных
+X, y = make_blobs(n_samples=200, centers=4, random_state=0)
+
+# Выбор признаков для визуализации
+feature_indices = input("Введите индексы признаков через запятую (например, 0,1,2): ").split(",")
+feature_indices = [int(idx) for idx in feature_indices]
+
+# K-Means
+kmeans = KMeans(n_clusters=4)
+kmeans_labels = kmeans.fit_predict(X)
+
+# Agglomerative clustering
+agg_clustering = AgglomerativeClustering(n_clusters=4)
+agg_labels = agg_clustering.fit_predict(X)
+
+# DBSCAN
+dbscan = DBSCAN(eps=0.5, min_samples=5)
+dbscan_labels = dbscan.fit_predict(X)
+
+# Визуализация результатов в сокращенном пространстве признаков
+X_reduced = X[:, feature_indices]
+
+fig = px.scatter(x=X_reduced[:, 0], y=X_reduced[:, 1], color=kmeans_labels)
+fig.update_layout(title="K-Means Clustering")
+fig.show()
+
+fig = px.scatter(x=X_reduced[:, 0], y=X_reduced[:, 1], color=agg_labels)
+fig.update_layout(title="Agglomerative Clustering")
+fig.show()
+
+fig = px.scatter(x=X_reduced[:, 0], y=X_reduced[:, 1], color=dbscan_labels)
+fig.update_layout(title="DBSCAN Clustering")
+fig.show()
